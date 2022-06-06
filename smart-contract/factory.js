61f0551b2a8bd1140d52039b5eaead9619eb7c02
nhanlinh1478 <luongtrongnhan1082000@gmail.com>
2022-06-06 17:17:59 +0700

    update_file

--- smart-contract/factory.js
+++ smart-contract/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x2Bec4B5E67FE9e6Ba5768D83d49a71A60067B813"
+  "0x8d35874292FE8b3E8745B080eF7C2A0253dDa493"
 );
 
 export default instance;
